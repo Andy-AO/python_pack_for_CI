c39e0a8671391fcdc8bbe07f26eefbac4833960d
zen96285 <zen96285@gmail.com>
2021-09-17 11:47:09 +0800

    1. 文档中包含构造方法 2. 注释掉空数组行（# exclude_patterns = []）

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode','sphinx.ext.napoleon']
-
+autoclass_content = "both"
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -44,7 +44,7 @@ language = 'zh-CN'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+# exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
