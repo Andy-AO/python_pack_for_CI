fd351b7122e3fc017db8b33cf638e44a3b96dec2
zen96285 <zen96285@gmail.com>
2021-09-17 12:54:40 +0800

    测试Node类的构造方法

--- tree_test.py
+++ tree_test.py
@@ -3,10 +3,18 @@ import pytest
 from tree import Node
 
 
+def test_get_content():
+    the_node = Node(title_level=1, title='the title', content='the content')
+    # 当get_content方法获取的对象被改变时，对象不变
+    the_node.get_content()[0] = 'new content'
+    assert the_node.get_content() == ['the content']
+
+
 class TestConstruction:
     def test_normal(self):
         the_node = Node(title_level=1, title='the title', content='the content')
         assert the_node.get_content() == ['the content']
+        # 在这里也顺便测试了get_title()和get_content()的基本功能
         assert the_node.get_title() == 'the title'
         assert the_node.get_title_level() == 1
 
